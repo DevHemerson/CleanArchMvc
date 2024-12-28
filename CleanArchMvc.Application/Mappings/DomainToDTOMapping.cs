a4344b9b9ab701fb0c933b2fcad3b0f0f9a6ce49
Hemerson Damasceno <hemersondamasceno90@gmail.com>
2024-12-28 15:43:26 -0300

    Criação e mapeamento das DTOs utilizando mapster

---
+++ CleanArchMvc.Application/Mappings/DomainToDTOMapping.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Entities;
+using Mapster;
+
+namespace CleanArchMvc.Application.Mappings;
+public static class DomainToDTOMapping
+{
+    public static void ConfigureMapppings()
+    {
+        TypeAdapterConfig<Category, CategoryDTO>.NewConfig();
+        TypeAdapterConfig<Product, ProductDTO>.NewConfig();
+    }
+}
